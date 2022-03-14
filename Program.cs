c13489960c3b57a4f171ad85144f99661e2eef20
chakravarthy <m.chakra473@gmail.com>
2022-03-14 17:18:22 +0530

    added play the game with 2 players to uc7

--- Program.cs
+++ Program.cs
@@ -8,11 +8,12 @@ namespace snake_and_ladder
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("snake and ladder played with single player at start 0");
-            dice();
-            option();
-            win();
-            winexact();
-            dicecount();    
+            //dice();
+            //option();
+            //win();
+            //winexact();
+            //dicecount();
+            twoplayers();
         }
         public static void dice()
         {
@@ -168,6 +169,183 @@ namespace snake_and_ladder
             }
             Console.WriteLine("the player reached the winning position exact 100!");
             Console.WriteLine("number of times dice rolled are:"+dice1);
+
+        }
+        public static void twoplayers()
+        {
+            Random random = new Random();
+           int player1 = 0;
+           int player2 = 0;
+            int dice1 = 0;
+            while(player1!=100&&player2!=100)
+            {
+                
+                int option = random.Next(0, 3);
+                while(option == 1&&player1!=100)
+                {
+                    switch (option)
+                    {
+
+                        case 1:
+                            int dice = random.Next(1, 7);
+                            Console.WriteLine("player 1 got ladder and moves forward by " + dice);
+                            player1 = player1 + dice;
+                            dice1++;
+                            if (player1 > 100)
+                            {
+                                player1 -= dice;
+                            }
+                            Console.WriteLine("player 1 position is " + player1);
+
+                            break;
+                        case 2:
+                            dice = random.Next(1, 7);
+                            Console.WriteLine("player 1 got snake moves backward by " + dice);
+                            Console.WriteLine("player 1 position is " + player1);
+                            player1 = player1 - dice;
+                            dice1++;
+                            if (player1 < 0)
+                            {
+                                player1 = 0;
+                            }
+                            Console.WriteLine("player 1 position is " + player1);
+                            ;
+                            break;
+                        default:
+                            Console.WriteLine("player 1 got no play and remains in same position");
+                            Console.WriteLine("player 1 position is " + player1);
+                            dice1++;
+                            break;
+
+                    }
+                    option = random.Next(0, 3);
+
+
+                }
+
+                switch (option)
+                {
+
+                    case 1:
+                        int dice = random.Next(1, 7);
+                        Console.WriteLine("player 1 got ladder and moves forward by " + dice);
+                        Console.WriteLine("player 1 position is " + player1);
+                        player1 = player1 + dice;
+                        dice1++;
+                        if (player1 > 100)
+                        {
+                            player1 -= dice;
+                        }
+                        break;
+                    case 2:
+                        dice = random.Next(1, 7);
+                        Console.WriteLine("player 1 got snake moves backward by " + dice);
+                        Console.WriteLine("player 1 position is " + player1);
+                        player1 = player1 - dice;
+                        dice1++;
+                        if (player1 < 0)
+                        {
+                            player1 = 0;
+                        }
+                        ;
+                        break;
+                    default:
+                        Console.WriteLine("player 1 got no play and remains in same position");
+                        Console.WriteLine("player 1 position is " + player1);
+                        dice1++;
+                        break;
+
+                }
+                  //player 2 plays
+                option = random.Next(0, 3);
+                while (option == 1&&player2!=100)
+                {
+                    switch (option)
+                    {
+
+                        case 1:
+
+                            int dice = random.Next(1, 7);
+                            Console.WriteLine("player 2 got ladder and moves forward by " + dice);
+                            Console.WriteLine("player 2 position is " + player2);
+                            player2 = player2 + dice;
+                            dice1++;
+                            if (player2 > 100)
+                            {
+                                player2 -= dice;
+                            }
+
+
+                            break;
+                        case 2:
+                            dice = random.Next(1, 7);
+                            Console.WriteLine("player 2 got snake moves backward by " + dice);
+                            Console.WriteLine("player 2 position is " + player2);
+                            player2 = player2 - dice;
+                            dice1++;
+                            if (player2 < 0)
+                            {
+                                player2 = 0;
+                            }
+                       ;
+                            break;
+                        default:
+                            Console.WriteLine("player 2 got no play and remains in same position");
+                            Console.WriteLine("player 2 position is " + player2);
+                            dice1++;
+                            break;
+
+                    }
+                    option=random.Next(0, 3);
+                }
+                switch (option)
+                {
+
+                    case 1:
+   
+                            int dice = random.Next(1, 7);
+                            Console.WriteLine("player 2 got ladder and moves forward by " + dice);
+                            Console.WriteLine("player 2 position is " + player2);
+                        player2 = player2 + dice;
+                            dice1++;
+                            if (player2 > 100)
+                            {
+                                player2 -= dice;
+                            }
+                            
+                        
+                        break;
+                    case 2:
+                        dice = random.Next(1, 7);
+                        Console.WriteLine("player 2 got snake moves backward by " + dice);
+                        Console.WriteLine("player 2 position is " + player2);
+                        player2 = player2 - dice;
+                        dice1++;
+                        if (player2 < 0)
+                        {
+                            player2 = 0;
+                        }
+                       ;
+                        break;
+                    default:
+                        Console.WriteLine("player 2 got no play and remains in same position");
+                        Console.WriteLine("player 2 position is " + player2);
+                        dice1++;
+                        break;
+
+                }
+
+
+            }
+            if(player2==100)
+            {
+                Console.WriteLine("player 2 wins the game!!!");
+            }
+            else
+            {
+                Console.WriteLine("player 1 wins the game!!!");
+            }
+            Console.WriteLine("number of times dice played was :" + dice1);
 
         }
     }
