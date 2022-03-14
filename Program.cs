fe3e5a2032e275a40d0f12dbef10f137a731248f
chakravarthy <m.chakra473@gmail.com>
2022-03-14 12:52:13 +0530

    added snake and ladder player1 rolls a dice logic to uc2

--- Program.cs
+++ Program.cs
@@ -8,6 +8,13 @@ namespace snake_and_ladder
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("snake and ladder played with single player at start 0");
+            dice();
+        }
+        public static void dice()
+        {
+            Random random = new Random();
+            int dice =random.Next(1,7);
+            Console.WriteLine("player rolls the dice and got:"+dice);
         }
     }
 }
